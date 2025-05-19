e61974c7752d3842b260940a901591509c2916b3
Igor Fernandes <igorv1286@gmail.com>
2025-05-19 18:39:22 -0300

    Objeto Unitário

---
+++ Singleton/Singleton/LogSingleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton
+{
+    public class LogSingleton
+    {
+        private static LogSingleton? Instance;
+
+        private LogSingleton()
+        {
+
+        }
+
+        public static LogSingleton GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new();
+
+                return Instance;
+            }
+        }
+
+        public void Write(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
