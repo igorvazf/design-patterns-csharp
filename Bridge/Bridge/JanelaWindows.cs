691a29ba8330632aabe8d24c63de5f2d6d4c72ab
Igor Fernandes <igorv1286@gmail.com>
2026-01-05 21:34:42 -0300

    Ponte

---
+++ Bridge/Bridge/JanelaWindows.cs
@@ -0,0 +1,15 @@
+﻿namespace Bridge
+{
+    public class JanelaWindows : IJanelaBridge
+    {
+        public void desenharBotao(string titulo)
+        {
+            Console.WriteLine(titulo + " - Botão Windows");
+        }
+
+        public void desenharJanela(string titulo)
+        {
+            Console.WriteLine(titulo + " - Janela Windows");
+        }
+    }
+}
